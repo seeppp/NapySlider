5d536bd82e42439e4f5149586623200ed5c1df21
Jonas Schoch <jonas.schoch@naptics.ch>
2015-12-12 13:15:14 +0100

    Initial Commit

---
+++ NapySlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NapySlider
+//
+//  Created by Jonas Schoch on 12.12.15.
+//  Copyright © 2015 naptics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
